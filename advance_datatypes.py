f016c55d21299104abfe97eb185d926164a4b6f0
HitmanBoi <99242256+HitmanBoi@users.noreply.github.com>
2024-02-01 18:23:19 +0000

    Adv_DTP

---
+++ advance_datatypes.py
@@ -0,0 +1,15 @@
+#list 
+a = [1,2,2,3,4,5]
+
+#tuple
+b= (1,2,2,3,4,5)
+
+#dictionary
+c= {'one':1,'two':2,'three':3,'four':4,'five':5}
+
+#set
+d= {1,2,2,3,4,5}
+
+print(type(a),type(b),type(c),type(d))
+
+print(a,d)
