b2eacecd278c8e52319881d2cc7402217e9467d5
HitmanBoi <99242256+HitmanBoi@users.noreply.github.com>
2024-02-01 18:28:17 +0000

    Typecasting

---
+++ typecasting.py
@@ -0,0 +1,16 @@
+a = 1
+b = 1.33
+c = "Hitman"
+
+#casting into string
+print(str(a))
+print(str(b))
+print(str(c))
+
+#casting into int
+print(str(b))
+#string cannot be casted into int untill it contains non numerical values
+
+#casting into float
+print(float(a))
+
